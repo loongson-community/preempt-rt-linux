062342ee560a72d35ec6323dc1766fb767d1055d
Wu Zhangjin <wuzj@lemote.com>
2009-06-30 12:21:55 +0800

    [MIPS] pm,hibernation: only save pages in system ram
    when using hibernation(STD) with CONFIG_FLATMEM in linux-mips-64bit, it
    fails for the current mips-specific hibernation implementation save the
    pages in all of the memory space(except the nosave section) and make
    there will be not enough memory left to the STD task itself, and then
    fail. in reality, we only need to save the pages in system rams.
    
    here is the reason why it fail:
    
    kernel/power/snapshot.c:
    
    static void mark_nosave_pages(struct memory_bitmap *bm)
    {
    		...
    		if (pfn_valid(pfn)) {
    			...
    		}
    }
    
    arch/mips/include/asm/page.h:
    
    	...
    	#ifdef CONFIG_FLATMEM
    
    	#define pfn_valid(pfn)		((pfn) >= ARCH_PFN_OFFSET && (pfn) < max_mapnr)
    
    	#elif defined(CONFIG_SPARSEMEM)
    
    	/* pfn_valid is defined in linux/mmzone.h */
    	...
    
    we can rewrite pfn_valid(pfn) to fix this problem, but I really do not
    want to touch such a widely-used macro, so, I used another solution:
    
    static struct page *saveable_page(struct zone *zone, unsigned long pfn)
    {
    	...
    	if ( .... pfn_is_nosave(pfn)
    		return NULL;
    	...
    }
    
    and pfn_is_nosave is implemented in arch/mips/power/cpu.c, so, hacking
    this one is better.
    
    Signed-off-by: Wu Zhangjin <wuzj@lemote.com>

--- arch/mips/power/cpu.c
+++ arch/mips/power/cpu.c
@@ -10,6 +10,7 @@
 #include <asm/suspend.h>
 #include <asm/fpu.h>
 #include <asm/dsp.h>
+#include <asm/bootinfo.h>
 
 static u32 saved_status;
 struct pt_regs saved_regs;
@@ -34,10 +35,26 @@ void restore_processor_state(void)
 		restore_dsp(current);
 }
 
+int pfn_in_system_ram(unsigned long pfn)
+{
+	int i;
+
+	for (i = 0; i < boot_mem_map.nr_map; i++) {
+		if (boot_mem_map.map[i].type == BOOT_MEM_RAM) {
+			if ((pfn >= (boot_mem_map.map[i].addr >> PAGE_SHIFT)) &&
+				((pfn) < ((boot_mem_map.map[i].addr +
+					boot_mem_map.map[i].size) >> PAGE_SHIFT)))
+				return 1;
+		}
+	}
+	return 0;
+}
+
 int pfn_is_nosave(unsigned long pfn)
 {
 	unsigned long nosave_begin_pfn = PFN_DOWN(__pa(&__nosave_begin));
 	unsigned long nosave_end_pfn = PFN_UP(__pa(&__nosave_end));
 
-	return	(pfn >= nosave_begin_pfn) && (pfn < nosave_end_pfn);
+	return ((pfn >= nosave_begin_pfn) && (pfn < nosave_end_pfn))
+			|| !pfn_in_system_ram(pfn);
 }
