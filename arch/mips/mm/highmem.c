2bb9fc448ab8fa79362d10fb1205bec44007d6b9
Wu Zhangjin <wuzhangjin@gmail.com>
2009-09-17 13:23:18 +0800

    [RT] add mips specific RT_PREEMPT support
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- arch/mips/mm/highmem.c
+++ arch/mips/mm/highmem.c
@@ -12,9 +12,9 @@ void *__kmap(struct page *page)
 {
 	void *addr;
 
-	might_sleep();
 	if (!PageHighMem(page))
 		return page_address(page);
+	might_sleep();
 	addr = kmap_high(page);
 	flush_tlb_one((unsigned long)addr);
 
