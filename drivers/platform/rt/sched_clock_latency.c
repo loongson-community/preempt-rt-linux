621482fc542f0a3488d17bafd271eac958f3cfba
Wu Zhangjin <wuzhangjin@gmail.com>
2010-03-14 13:53:26 +0800

    RT: add a sched_clock latency driver
    This driver is designed to measure the latency of a clock getting
    function.
    
    Usage:
    
     o Compile this module
       ensure CONFIG_SCHED_CLOCK_LAT=y|m
     o Load it if compiled as module
       $ modprobe sched_clock_latency
     o Create a char device as the kernel message prompt
       $ mdesg | grep mknod
       $ mknod /dev/sched_clock_latency c 253 0
     o Input the measurement loop and the number of the function
       $ echo <loop> <func_num> > /dev/sched_clock_latency
       loop: the numbers need to calling sched_clock to accumulate the
       result.
       func_num: sched_clock, 0; getnstimeofday: 1.
     o Wait until exit
     o Get the result from /dev/sched_clock_latency
      $ cat /dev/sched_clock_latency
    
    Note: This driver may introduce some overhead to calculate the max and
    min number, for example, when I don't calculate the max and min number,
        the avg latency is about 50, but when calculating them, the avg
        latency is about 80, so, there is about 30 ns costed on calculating
        the max and min number. therefore, by default, we will not calculate
        them(min always be -1 and max always be 0), if you need them, please
        define CALCULATE_MAX_MIN in the header of this driver.
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

---
+++ drivers/platform/rt/sched_clock_latency.c
@@ -0,0 +1,318 @@
+/*
+ *  sched_clock() Latency Driver
+ *
+ *  This driver is designed for testing the duration need to call sched_clock()
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>	/* for put_user */
+#include <linux/sched.h>	/* for sched_clock */
+
+/*
+ *  Prototypes - this would normally go in a .h file
+ */
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+
+#define SUCCESS 0
+#define FAILURE 1
+/* Dev name as it appears in /proc/devices   */
+#define DEVICE_NAME "sched_clock_latency"
+/* Max length of the message from the device */
+#define BUF_LEN 1024
+
+/*
+ * Global variables are declared as static, so are global within the file.
+ */
+
+static int Major;		/* Major number assigned to our device driver */
+static int Device_Open;		/* Is device open?
+				 * Used to prevent multiple access to device */
+static char msg[BUF_LEN];	/* The msg the device will give when asked */
+static char *msg_Ptr;
+
+static const struct file_operations fops = {
+	.read = device_read,
+	.write = device_write,
+	.open = device_open,
+	.release = device_release
+};
+
+/*
+ * This function is called when the module is loaded
+ */
+static __init int sched_clock_latency_init(void)
+{
+	Major = register_chrdev(0, DEVICE_NAME, &fops);
+
+	if (Major < 0) {
+		printk(KERN_ALERT "Registering char device failed with %d\n",
+		       Major);
+		return Major;
+	}
+
+	printk(KERN_INFO "I was assigned major number %d. To talk to\n", Major);
+	printk(KERN_INFO "the driver, create a dev file with\n");
+	printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major);
+	printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
+	printk(KERN_INFO "the device file.\n");
+	printk(KERN_INFO "Remove the device file and module when done.\n");
+
+	return SUCCESS;
+}
+
+/*
+ * This function is called when the module is unloaded
+ */
+static __exit void sched_clock_latency_exit(void)
+{
+	/*
+	 * Unregister the device
+	 */
+	unregister_chrdev(Major, DEVICE_NAME);
+}
+
+/*
+ * Methods
+ */
+
+static u64 loop, total;
+unsigned int min = UINT_MAX, max = 0, avg = 0;
+int func_no;
+
+enum {
+	CLOCK_START = -1,
+	SCHED_CLOCK,
+	GETNSTIMEOFDAY,
+	CLOCK_END,
+};
+
+#define CLOCK_NUM (CLOCK_END - CLOCK_START)
+
+static inline int valid_func(int func_no)
+{
+	return func_no > CLOCK_START && func_no < CLOCK_END;
+}
+
+static inline int valid_loop(u64 loop)
+{
+	return loop >= 2 && loop <= 10000000;
+}
+
+static char func_name[CLOCK_NUM][50] = {
+	"sched_clock",
+	"getnstimeofday",
+	"unknown",
+};
+
+#ifdef CALCULATE_MAX_MIN
+static inline unsigned int get_clock(int func_no)
+{
+	unsigned int diff;
+	static u64 t0, t1;
+	static struct timespec tv0;
+	struct timespec tv, diff_tv;
+
+	switch (func_no) {
+	case GETNSTIMEOFDAY:
+		if (tv0.tv_nsec || tv0.tv_sec) {
+			getnstimeofday(&tv);
+			diff_tv = timespec_sub(tv, tv0);
+			diff = timespec_to_ns(&diff_tv);
+			tv0 = tv;
+		} else
+			getnstimeofday(&tv0);
+		break;
+	case SCHED_CLOCK:
+		if (t0) {
+			t1 = sched_clock();
+			diff = t1 - t0;
+			t0 = t1;
+		} else
+			t0 = sched_clock();
+		break;
+	default:
+		printk(KERN_ALERT "Sorry, no such function.\n");
+		break;
+	}
+	return diff;
+}
+#else
+static inline unsigned int get_clock(int func_no)
+{
+	static u64 t;
+	struct timespec tv;
+
+	switch (func_no) {
+	case GETNSTIMEOFDAY:
+		getnstimeofday(&tv);
+		t = timespec_to_ns(&tv);
+		break;
+	case SCHED_CLOCK:
+		t = sched_clock();
+		break;
+	default:
+		printk(KERN_ALERT "Sorry, no such function.\n");
+		break;
+	}
+	return t;
+}
+#endif
+
+static void calculate_latency(int func_no)
+{
+	u64 t0, t1;
+	int i;
+	unsigned int __maybe_unused diff;
+
+	/* Init */
+	max = 0;
+	min = UINT_MAX;
+
+	local_irq_disable();
+	t0 = sched_clock();
+	(void)get_clock(func_no);
+	for (i = 0; i < loop - 1; i++) {
+#ifdef CALCULATE_MAX_MIN
+		diff = get_clock(func_no);
+		if (diff > max)
+			max = diff;
+		else if (diff < min)
+			min = diff;
+#else
+		(void)get_clock(func_no);
+#endif
+	}
+	t1 = sched_clock();
+	local_irq_enable();
+
+	total = t1 - t0;
+	avg = total;
+	do_div(avg, loop);
+}
+
+/*
+ * Called when a process tries to open the device file, like
+ * "cat /dev/mycharfile"
+ */
+static int device_open(struct inode *inode, struct file *file)
+{
+	if (Device_Open)
+		return -EBUSY;
+
+	Device_Open++;
+
+	if (loop == 0)
+		sprintf(msg,
+			"Please set the loop and clock function via /dev/%s,\n"
+			"i.e. $ echo <loop> <clock_func_num> > /dev/%s\n"
+			"<loop>: any unsigned number >= 2, <= 10000000\n"
+			"<clock_func_num>:\n"
+			"\t0: sched_clock\n"
+			"\t1: getnstimeofday\n", DEVICE_NAME, DEVICE_NAME);
+	else
+		sprintf(msg,
+			"func: %s, loop: %lld, total: %lld,"
+			" avg: %d, max: %d, min: %d\n",
+			func_name[func_no], loop, total, avg, max, min);
+
+	msg_Ptr = msg;
+	try_module_get(THIS_MODULE);
+
+	return SUCCESS;
+}
+
+/*
+ * Called when a process closes the device file.
+ */
+static int device_release(struct inode *inode, struct file *file)
+{
+	Device_Open--;		/* We're now ready for our next caller */
+
+	/*
+	 * Decrement the usage count, or else once you opened the file, you'll
+	 * never get get rid of the module.
+	 */
+	module_put(THIS_MODULE);
+
+	return 0;
+}
+
+/*
+ * Called when a process, which already opened the dev file, attempts to
+ * read from it.
+ */
+static ssize_t device_read(struct file *filp,	/* see include/linux/fs.h   */
+			   char *buffer,	/* buffer to fill with data */
+			   size_t length,	/* length of the buffer     */
+			   loff_t *offset)
+{
+	/*
+	 * Number of bytes actually written to the buffer
+	 */
+	int bytes_read = 0;
+
+	/*
+	 * If we're at the end of the message,
+	 * return 0 signifying end of file
+	 */
+	if (*msg_Ptr == 0)
+		return 0;
+
+	/*
+	 * Actually put the data into the buffer
+	 */
+	while (length && *msg_Ptr) {
+
+		/*
+		 * The buffer is in the user data segment, not the kernel
+		 * segment so "*" assignment won't work.  We have to use
+		 * put_user which copies data from the kernel data segment to
+		 * the user data segment.
+		 */
+		put_user(*(msg_Ptr++), buffer++);
+
+		length--;
+		bytes_read++;
+	}
+
+	/*
+	 * Most read functions return the number of bytes put into the buffer
+	 */
+	return bytes_read;
+}
+
+/*
+ * Called when a process writes to dev file: echo "hi" > /dev/hello
+ */
+static ssize_t
+device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
+{
+	if (sscanf(buff, "%lld %d", &loop, &func_no) >= 1) {
+		if (!valid_loop(loop))
+			printk(KERN_ERR
+			       "Invalid loop no: %lld, "
+			       "loop must >= 2, <= 10000000\n", loop);
+		else if (!valid_func(func_no))
+			printk(KERN_ERR "Unknown clock function\n");
+		else {
+			printk(KERN_ALERT "Please wait, is calcaulating...\n");
+			calculate_latency(func_no);
+			printk(KERN_ALERT "Calculation is finished, please \n");
+			printk(KERN_ALERT "get the result via cat /dev/%s \n",
+			       DEVICE_NAME);
+		}
+	}
+	return strlen(buff);
+}
+
+module_init(sched_clock_latency_init);
+module_exit(sched_clock_latency_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Wu Zhangjin <wuzhangjin@gmail.com>");
+MODULE_DESCRIPTION("sched_clock latency driver");
